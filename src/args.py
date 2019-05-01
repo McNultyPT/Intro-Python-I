ea57ac1b87ac100b5082235f1349980f3f31a293
McNultyPT <mcnultypt@gmail.com>
2019-04-30 17:18:56 -0700

    continuing on args

--- src/args.py
+++ src/args.py
@@ -16,18 +16,18 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+a = [7, 6, 5, 4]
+
 def f2(*num):
     return sum(num)
 
-print(f2(1,))                    # Should print 1
+print(f2(1,))                   # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-a = [7, 6, 5, 4]
-
 # What thing do you have to add to make this work?
-print(f2([a])    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -35,15 +35,14 @@ print(f2([a])    # Should print 22
 
 # YOUR CODE HERE
 
-def f3(*num):
-        if len(num) <= 1:
-                return num + 1
-        else:
-                return sum(num)
-
+def f3(num, multi=0):
+    if multi:
+        return num + multi
+    else:
+        return num + 1
+        
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
-
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
--- src/classes.py
+++ src/classes.py
@@ -6,6 +6,7 @@
 class LatLon:
     lat = 0
     lon = 0
+    
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
